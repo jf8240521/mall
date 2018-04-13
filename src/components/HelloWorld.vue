638937831b9fd34625f8f7465cc56788a75ced7b
wj <1248136872@qq.com>
2018-04-13 12:19:21 +0800

    mall

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <mt-button type="default">default</mt-button>
+    <mt-button type="primary">primary</mt-button>
+    <mt-button type="danger">danger</mt-button>
+    <mt-button icon="back">back</mt-button>
+    <mt-button icon="more">更多</mt-button>
+    <mt-button plain>plain</mt-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'mint-ui'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  component: { Button }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
